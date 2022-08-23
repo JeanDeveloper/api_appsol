3e07902603f4743c412d1326b74d4499a030dfa6
Jean CHUNGA Sandoval <jchunga@SOLMAR.DOM>
2022-08-23 10:27:25 -0500

    actualizando al 23/08/2022

--- apps/cargos/api/serializers.py
+++ apps/cargos/api/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 
-
 class CargosSerializer(serializers.Serializer):
     codigo     = serializers.DecimalField(max_digits=18, decimal_places=0)
     cargo      = serializers.CharField(max_length=255, allow_null=True, allow_blank=True)
