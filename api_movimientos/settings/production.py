7e0cf4ef7149c409b62f143b21cae9394660de5a
Jean Carlo <jecachusa.1996@gmail.com>
2022-10-28 12:18:02 -0500

    actualizando al 28/19/2022

--- api_movimientos/settings/production.py
+++ api_movimientos/settings/production.py
@@ -4,7 +4,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*' ]
+ALLOWED_HOSTS = ['190.116.178.163', 'http://190.116.178.163' ]
 
 DATABASES = {
 
